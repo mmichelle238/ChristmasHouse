dbf4f3b74223f1eb7291ffe91e3eccc86f8a1877
mmorris1 <mmorris1@cs.washington.edu>
2020-10-03 15:03:15 -0700

    updated README

--- Source/CHApi/queries.js
+++ Source/CHApi/queries.js
@@ -2,7 +2,7 @@ const Pool   = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'ChristmasHouse',
+    database: 'christmashouse',
     password: 'Panda123',
     port: 5432,
   })
